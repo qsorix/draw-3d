8b25c15f6a2cc2453d115e5af34d5a79f9260e47
Tomasz Rydzynski <qsorix@rydznet.pl>
2015-01-05 16:43:45 +0100

    Add rotation around camera using mouse wheel

--- tools/mousezoom.py
+++ tools/mousezoom.py
@@ -7,7 +7,10 @@ class ToolMouseZoom(Tool):
     WHEELDOWN = 5
 
     # each zoom takes you closer by this many percent
-    MOVESPEED = 0.04
+    ZOOM_SPEED = 0.04
+
+    # in radians/pixel
+    ROTATION_SPEED = 0.003
 
     # move camera at least by this length, so it will pass through objects near
     # the screen plane
@@ -16,10 +19,13 @@ class ToolMouseZoom(Tool):
     # by how much to move when mouse points at void
     FREE_MOVE = 10
 
-    def mouseDown(self, button, pos):
-        pass
+    def reset(self):
+        self._rotation_start_angle = None
+        self._rotation_start_angle_vert = None
+        self._rotation_point = None
+        self._start_mouse_pos = None
 
-    def mouseUp(self, button, pos):
+    def _get_mouse_target(self, pos):
         objects = self.wnd.get_objects_pointed_at(*pos)
         if objects:
             destination = objects[0][1]
@@ -27,7 +33,30 @@ class ToolMouseZoom(Tool):
             view_ray = self.wnd.get_view_ray(*pos)
             destination = self.wnd.camera + view_ray.v*self.FREE_MOVE
 
-        move = self.MOVESPEED*funcs.vector_from_to(self.wnd.camera, destination)
+        return destination
+
+    def mouseDown(self, button, pos):
+        if button != 2:
+            return
+
+        self._rotation_start_angle = self.wnd.camera_angle
+        self._rotation_start_angle_vert = self.wnd.camera_angle_vert
+        self._start_mouse_pos = pos
+
+    def mouseMotion(self, buttons, pos, rel):
+        if not self._start_mouse_pos:
+            return
+
+        angle = self.ROTATION_SPEED * (pos[0] - self._start_mouse_pos[0])
+        angle_vert = self.ROTATION_SPEED * (pos[1] - self._start_mouse_pos[1])
+
+        self.wnd.camera_angle = self._rotation_start_angle - angle
+        self.wnd.camera_angle_vert = self._rotation_start_angle_vert - angle_vert
+
+    def mouseUp(self, button, pos):
+        destination = self._get_mouse_target(pos)
+
+        move = self.ZOOM_SPEED*funcs.vector_from_to(self.wnd.camera, destination)
         if funcs.length(move) < self.MIN_MOVE:
             move = funcs.unit(move) * self.MIN_MOVE
         if button == self.WHEELUP:
